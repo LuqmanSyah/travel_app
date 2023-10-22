1dc401aee385b7a5d27fae7a94fca3f8961a0ffe
(luqman syahreno) <eno.luqman@gmail.com>
2023-10-22 11:08:06 +0700

    Make Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,18 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-    title: "Travel",
-    description: "Travel UI/UX App for Camping",
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>
-                <Navbar />
-                <main className="relative overflow-hidden">{children}</main>
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
