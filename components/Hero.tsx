ebe2d0f09fa677b183beae377470ff7a2f3cdb65
(luqman syahreno) <eno.luqman@gmail.com>
2023-11-05 00:05:12 +0700

    Change hero.tsx and create guide.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <Image src="/camp.svg" alt="camp" width={50} height={50} className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]" />
         <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          We want to be on each of your journeys seeking the statisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
+          We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
@@ -32,7 +32,7 @@ const Hero = () => {
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
           <Button type="button" title="Download App" variant="btn_green" />
-          <Button type="button" title="How we work" icon="/play.svg" variant="btn_white_text" />
+          <Button type="button" title="How we work?" icon="/play.svg" variant="btn_white_text" />
         </div>
       </div>
 
